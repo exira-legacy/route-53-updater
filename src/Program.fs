0b7cb9acaf694c879695a8ec7ba96c1c8d2af671
David Cumps <david@cumps.be>
2015-11-03 16:33:16 +0100

    Determine Public hostname (R53-1), Find Zone (R53-2) and Update Zone (R53-3)

--- src/Program.fs
+++ src/Program.fs
@@ -2,19 +2,34 @@
 open Amazon.EC2.Util
 open Exira.ErrorHandling
 open System
+open Amazon.Route53
+open Amazon.Route53.Model
+open Amazon
+open Amazon.Runtime
 
 type UpdaterConfig = YamlConfig<"Updater.yaml">
 let updaterConfig = UpdaterConfig()
+updaterConfig.Load "Updater.yaml"
 
 type Errors =
     | FailedToRetrieveEc2MetaData of exn
     | FailedToRetrieveNetworkInterface
+    | FailedToRetrieveRoute53Zone of exn
+    | FailedToRetrieveRoute53ResourceRecordSet
+    | FailedToRetrieveRoute53ResourceRecord
+    | Route53ResourceRecordUpToDate
+    | FailedToUpdateRoute53Zone of exn
 
 let format errors =
     let formatError error =
         match error with
         | FailedToRetrieveEc2MetaData _ -> sprintf "Could not retrieve EC2 meta data."
         | FailedToRetrieveNetworkInterface -> sprintf "No network interfaces were found."
+        | FailedToRetrieveRoute53Zone _ -> sprintf "Could not retrieve Route 53 zone."
+        | FailedToRetrieveRoute53ResourceRecordSet -> sprintf "Route 53 Resource Record Set was not found."
+        | FailedToRetrieveRoute53ResourceRecord -> sprintf "Route 53 Resource Record was not found."
+        | Route53ResourceRecordUpToDate -> sprintf "Route 53 Resource Record is up to date!"
+        | FailedToUpdateRoute53Zone _ -> sprintf "Could not update Route 53 zone."
 
     errors
     |> List.map formatError
@@ -43,17 +58,102 @@ let getNetworkInterface nics =
     | Some nic -> succeed nic
     | None -> fail [FailedToRetrieveNetworkInterface]
 
+let getRoute53ResourceRecordSet =
+    try
+        use route53Client =
+            new AmazonRoute53Client(
+                updaterConfig.Updater.AwsAccessKey,
+                updaterConfig.Updater.AwsSecretKey,
+                RegionEndpoint.GetBySystemName updaterConfig.Updater.Region)
+
+        let request =
+            ListResourceRecordSetsRequest(
+                HostedZoneId = updaterConfig.Updater.ZoneId,
+                StartRecordName = updaterConfig.Updater.DomainName,
+                StartRecordType = RRType.CNAME,
+                MaxItems = string 1)
+
+        let response = route53Client.ListResourceRecordSets request
+        let resourceRecordSet =
+            response.ResourceRecordSets
+            |> Seq.tryHead
+
+        let checkDomainName (resourceRecordSet: ResourceRecordSet) =
+            match resourceRecordSet.Name, resourceRecordSet.Type with
+            | n, t when n = updaterConfig.Updater.DomainName && t = RRType.CNAME -> succeed resourceRecordSet
+            | _ -> fail [FailedToRetrieveRoute53ResourceRecordSet]
+
+        match resourceRecordSet with
+        | Some resourceRecordSet -> checkDomainName resourceRecordSet
+        | None -> fail [FailedToRetrieveRoute53ResourceRecordSet]
+    with
+    | ex -> fail [FailedToRetrieveRoute53Zone ex]
+
+let printResourceRecordSet resourceRecordSet =
+    let printDebug (resourceRecordSet: ResourceRecordSet) =
+        printfn "Name: %A - Type: %A" resourceRecordSet.Name resourceRecordSet.Type
+        Seq.iter (fun (rr: ResourceRecord) -> printfn "RR: %s" rr.Value) resourceRecordSet.ResourceRecords
+
+    if updaterConfig.Updater.Debug then resourceRecordSet |> printDebug
+    else ()
+    resourceRecordSet
+
+let compareNicWithResourceRecordSet (nic: NetworkInterface) (resourceRecordSet: ResourceRecordSet) =
+    let resourceRecord =
+        resourceRecordSet.ResourceRecords
+        |> Seq.tryHead
+
+    match resourceRecord with
+    | Some rr when rr.Value.StartsWith nic.PublicHostname -> fail [Route53ResourceRecordUpToDate]
+    | None -> fail [FailedToRetrieveRoute53ResourceRecord]
+    | Some rr ->
+        rr.Value <- sprintf "%s." nic.PublicHostname
+        succeed resourceRecordSet
+
+let updateResourceRecordSet (resourceRecordSet: ResourceRecordSet) =
+    try
+        use route53Client =
+            new AmazonRoute53Client(
+                updaterConfig.Updater.AwsAccessKey,
+                updaterConfig.Updater.AwsSecretKey,
+                RegionEndpoint.GetBySystemName updaterConfig.Updater.Region)
+
+        let changes = [ new Change(ChangeAction.UPSERT, resourceRecordSet) ]
+        let changeBatch = ChangeBatch(Collections.Generic.List<Change>(changes))
+        changeBatch.Comment <- sprintf "Updating CNAME to match real public hostname."
+        let request = ChangeResourceRecordSetsRequest(updaterConfig.Updater.ZoneId, changeBatch)
+        let response = route53Client.ChangeResourceRecordSets request
+
+        succeed response
+    with
+    | ex -> fail [FailedToUpdateRoute53Zone ex]
+
 [<EntryPoint>]
 let main argv =
-    let route53Updater =
+    let determineNic() =
         getNetworkInterfaces
         |> map printNetworkInterfaces
         |> bind getNetworkInterface
 
-        // TODO: Go to route 53 and update if not the same
+    let determineResourceRecordSet() =
+        getRoute53ResourceRecordSet
+        |> map printResourceRecordSet
+
+    let determineNewResourceRecordSet =
+        match determineNic(), determineResourceRecordSet() with
+        | Success nic, Success resourceRecordSet -> compareNicWithResourceRecordSet nic resourceRecordSet
+        | Failure a', Success b' -> fail a'
+        | Success a', Failure b' -> fail b'
+        | Failure a', Failure b' -> fail (a' @ b')
+
+    let route53Updater =
+        determineNewResourceRecordSet
+        |> map printResourceRecordSet
+        |> bind updateResourceRecordSet
 
     match route53Updater with
-    | Success _ -> printfn "Updated"
+    | Success result ->
+        printfn "[%s] Route 53 Zone Updated!" result.ChangeInfo.Id
     | Failure errors ->
         let errorMessages = errors |> format
         printfn "%s" <| String.concat Environment.NewLine errorMessages
