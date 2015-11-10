947edd4ae9125a2d69a55b5f7fb52d9aa53e327d
Team City <info@exira.com>
2015-11-10 00:34:56 +0000

    Bump version to 1.1.11

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("route-53-updater")>]
 [<assembly: AssemblyProductAttribute("Exira.Route53Updater")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.Route53Updater is a console application which updates a Route 53 record with the current EC2 hostname")>]
-[<assembly: AssemblyVersionAttribute("1.0.10")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.10")>]
-[<assembly: AssemblyMetadataAttribute("githash","4c6f67740f6f3d2f507338581055c0bc49d45e42")>]
+[<assembly: AssemblyVersionAttribute("1.1.11")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.11")>]
+[<assembly: AssemblyMetadataAttribute("githash","a16e28c8e53195ef3cc9367c460e86920116f1e1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.10"
+    let [<Literal>] Version = "1.1.11"
