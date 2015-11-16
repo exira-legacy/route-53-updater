abf2c2ebd4df7f582ed940bd44b2897887fc6fed
David Cumps <david@cumps.be>
2015-11-16 08:45:31 +0100

    Add some more documentation

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -20,13 +20,13 @@ Exira.Route53Updater is a console application which updates a Route 53 record wi
  * Edit `Updater.yaml` using:
    * AwsAccessKey: `YOUR AWS ACCESS KEY`, e.g.: `AXIURT98PXVXJU7K...`
    * AwsSecretKey: `YOUR AWS SECRET KEY`, e.g.: `1Y9poTREdb/8j1234Tr...`
-   * Region: ```YOUR AWS REGION```, e.g.: ```eu-central-1`
+   * Region: `YOUR AWS REGION`, e.g.: `eu-central-1`
    * ZoneId: `YOUR ZONE ID`, e.g.: `ZRTPOCDERLDCO`
-   * DomainName: `YOUR DOMAIN`, e.g.: `www.example.org.` (Pay attention to the trailing .)
+   * DomainName: `YOUR DOMAIN`, e.g.: `www.example.org.` (Pay attention to the trailing `.`)
 
  * Run `route-53-updater.exe`
 
- * Your Route 53 record should now be updated with the public hostname of your EC2 instance
+ * Your `Route 53` record should now be updated with the public hostname of your EC2 instance
 
 ### Cloning
 
